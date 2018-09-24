0e2726c9641ec20dd6c72f7061626c6847e7a619
Dan Keefe <peritract@hotmail.co.uk>
2018-09-24 18:42:19 +0100

    Create writing-robot.py

---
+++ writing-robot.py
@@ -0,0 +1,22 @@
+import tweepy
+import os
+from time import sleep
+
+A_SECRET = os.environ['A_SECRET']
+A_TOKEN = os.environ['A_TOKEN']
+C_TOKEN = os.environ['C_TOKEN']
+C_SECRET = os.environ['C_SECRET']
+
+auth = tweepy.OAuthHandler(C_TOKEN, C_SECRET)
+auth.set_access_token(A_TOKEN, A_SECRET)
+
+API = tweepy.API(auth)
+
+if __name__ == "__main__":
+    while True:
+        #Get the last 50 tweets that match the criteria
+        for tweet in tweepy.Cursor(API.search, q='#amwriting OR #redditwriters OR #100daysofwriting', rpp=50).items(50):
+            if not tweet.retweeted:
+                tweet.retweet()
+                sleep(15)
+    sleep(60)
