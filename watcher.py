a347085dc041f8559addc93c4123c488dd9befa7
Peritract <peritract@hotmail.co.uk>
2018-10-14 15:10:45 +0100

    Tuning limits

--- watcher.py
+++ watcher.py
@@ -12,16 +12,18 @@ class Watcher(tweepy.StreamListener):
 
     def essential_maintenance(self):
         #follow all followers
+        let new_follows = 0
         for follower in tweepy.Cursor(self.api.followers).items():
-            if not follower.following:
+            if not follower.following and new_follows < 100:
                 follower.follow()
-            self.maintenance = datetime.datetime.now()
+                new_follows += 1
+        self.maintenance = datetime.datetime.now()
 
     def handle_queue(self):
         #If there's something in the queue and the last action was long enough ago, act.
         if len(self.queue) > 0 and self.check_time():
-            if len(self.queue) > 25:
-                self.queue = self.queue[-25:]
+            if len(self.queue) > 35:
+                self.queue = self.queue[-35:]
             tweet = self.queue.pop(0)
             try:
                 tweet.retweet()
@@ -46,5 +48,5 @@ class Watcher(tweepy.StreamListener):
     def on_error(self, error):
         #If something goes wrong, stop streaming, call for help
         self.functioning = False
-        self.api.update_status("Help me I am broken. @peritract")
+        self.api.update_status("Help me I am broken. @peritract @dmntdbttrfly")
         return False #disconnect
