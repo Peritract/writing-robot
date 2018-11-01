699d27a17b120cfc7d214e2cb2330226f6ec7132
Dan Keefe <peritract@hotmail.co.uk>
2018-11-01 21:31:35 +0000

    Increases the robustness of error handling by doing less of it

--- writing_watcher.py
+++ writing_watcher.py
@@ -238,10 +238,10 @@ class Watcher(tweepy.StreamListener):
         
     def on_error(self, error):
         # If twitter sends an error back from the stream
-        print(datetime.datetime.now(), "Error:", error.response.text)
+        print(datetime.datetime.now(), "Error:", error)
         if error.api_code == 144: #Status to be retweeted no longer exists - doesn't need to stop anything
             pass
-        elif error.api_code == 187:
+        elif error.api_code == 187: #Duplicate status
             pass
         else:
             self.running = False
