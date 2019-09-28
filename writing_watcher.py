a1eac22241ef5d0fd12137c3163f11f3ce6d5dfe
Dan Keefe <peritract@hotmail.co.uk>
2019-09-28 17:19:00 +0100

    move from 4 to 5 minute tweet delay

--- writing_watcher.py
+++ writing_watcher.py
@@ -30,7 +30,7 @@ class Watcher(tweepy.StreamListener):
         self.blocked = self.update_blocks()
         
         self.queue = [] #the tweets to be posted
-        self.retweet_delay = 240 #how long between tweets in seconds
+        self.retweet_delay = 300 #how long between tweets in seconds
         self.last_retweet = datetime.datetime.now() #The last time it tweeted
 
         #Tweets for specific days of the year
